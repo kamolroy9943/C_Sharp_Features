492b5c63fe4db317411d2bc501610c84aa868a21
kamol roy <kamol.roy9943@gmail.com>
2018-08-22 12:31:28 +0600

    Playing with Delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class MyClass
+    {
+        public delegate int Operation(int x);
+
+        public static int TernsForm(int value,Operation operation)
+        {
+            return operation(value);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = MyClass.TernsForm(20, CalCulation.Cuber);
+
+            Console.WriteLine(result);
+        }
+
+    }
+
+    public class CalCulation
+    {
+        public static int Squere(int value)
+        {
+            return value * value;
+        }
+        public static int Cuber(int value)
+        {
+            return value * value * value;
+        }
+    }
+}
